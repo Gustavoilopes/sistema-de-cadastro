1d37b8c429708e5a7e74f930f011e0f25c59bf5a
Gustavoilopes <gustavo.icaro.lopes@gmail.com>
2023-02-04 11:37:17 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Teste Proxima aula")
+//mais um console de teste
+console.log("Commit da proxima aula!")
+console.log("Enviando para o GitHub")
